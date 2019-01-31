903067f561d5c8d6e58bc44749a9124c4255edc8
alakovic1 <alakovic1@etf.unsa.ba>
2019-01-31 19:10:40 +0100

    kreiran GUI za registraciju korisnika

---
+++ src/sample/RegisterController.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class RegisterController {
+    public void onRegister(ActionEvent actionEvent) {
+    }
+}
--- src/sample/StartController.java
+++ src/sample/StartController.java
@@ -1,14 +1,48 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class StartController {
 
     public void onRegistration(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            Stage primaryStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/register.fxml"));
+            root = loader.load();
+            primaryStage.setTitle("Register");
+            primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            primaryStage.initModality(Modality.APPLICATION_MODAL);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onLogin(ActionEvent actionEvent) {
+        /*Parent root = null;
+        try {
+            Stage primaryStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            root = loader.load();
+            primaryStage.setTitle("Login");
+            primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            primaryStage.initModality(Modality.APPLICATION_MODAL);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
