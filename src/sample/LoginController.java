4bdfc68f20eee1af218c432d7f4daa9355a1b936
alakovic1 <alakovic1@etf.unsa.ba>
2019-07-23 17:57:11 +0200

    obrisano nepotrebno

--- src/sample/LoginController.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-import javafx.event.ActionEvent;
-
-public class LoginController {
-    public void onLogin(ActionEvent actionEvent) {
-    }
-}
--- src/sample/LoginnRegisterController.java
+++ src/sample/LoginnRegisterController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 
 public class LoginnRegisterController {
+
     public void onRegister(ActionEvent actionEvent) {
     }
 
