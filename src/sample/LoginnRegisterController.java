c985f6dd3b521be05d917abe25c1dcf43d56650b
alakovic1 <alakovic1@etf.unsa.ba>
2019-07-23 19:19:03 +0200

    nesto pocela...

--- src/sample/LoginnRegisterController.java
+++ src/sample/LoginnRegisterController.java
@@ -1,8 +1,18 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 
 public class LoginnRegisterController {
+    public TextField usernameLogin;
+    public TextField passwordLogin;
+    public TextField firstName;
+    public TextField lastName;
+    public TextField username;
+    public TextField adress;
+    public TextField email;
+    public TextField password;
 
     public void onRegister(ActionEvent actionEvent) {
     }
