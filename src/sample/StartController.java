e5a639a5ad3262324a74569623614daba429bea3
alakovic1 <alakovic1@etf.unsa.ba>
2019-07-23 17:18:19 +0200

    pocetak rada..

--- src/sample/StartController.java
+++ src/sample/StartController.java
@@ -13,29 +13,13 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class StartController {
 
-    public void onRegistration(ActionEvent actionEvent) {
+    public void onRegistrationLogin(ActionEvent actionEvent) {
         Parent root = null;
         try {
             Stage primaryStage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/register.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/loginnregister.fxml"));
             root = loader.load();
-            primaryStage.setTitle("Register");
-            primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-            primaryStage.initModality(Modality.APPLICATION_MODAL);
-            primaryStage.setResizable(false);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void onLogin(ActionEvent actionEvent) {
-        Parent root = null;
-        try {
-            Stage primaryStage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-            root = loader.load();
-            primaryStage.setTitle("Login");
+            primaryStage.setTitle("Register / Login");
             primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
             primaryStage.initModality(Modality.APPLICATION_MODAL);
             primaryStage.setResizable(false);
