d8d52d742547f5c2242483668bb6e1196abdc9ed
alakovic1 <alakovic1@etf.unsa.ba>
2019-07-25 00:47:18 +0200

    kreirana baza

---
+++ src/sample/Person.java
@@ -0,0 +1,80 @@
+package sample;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String adress;
+    private String email;
+    private String password;
+
+    public Person() {
+    }
+
+    public Person(int id, String firstName, String lastName, String username, String adress, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.adress = adress;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/sample/Vehicle.java
@@ -0,0 +1,100 @@
+package sample;
+
+public class Vehicle {
+    private int id;
+    private String name;
+    private String brand;
+    private String model;
+    private int nmbDoors;
+    private int nmbSeats;
+    private String engine;
+    private int available;
+    private int price;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(int id, String name, String brand, String model, int nmbDoors, int nmbSeats, String engine, int available, int price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.model = model;
+        this.nmbDoors = nmbDoors;
+        this.nmbSeats = nmbSeats;
+        this.engine = engine;
+        this.available = available;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNmbDoors() {
+        return nmbDoors;
+    }
+
+    public void setNmbDoors(int nmbDoors) {
+        this.nmbDoors = nmbDoors;
+    }
+
+    public int getNmbSeats() {
+        return nmbSeats;
+    }
+
+    public void setNmbSeats(int nmbSeats) {
+        this.nmbSeats = nmbSeats;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
