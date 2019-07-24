804c5da6b8898d100c1d5ab4d8ee772b65d94582
alakovic1 <alakovic1@etf.unsa.ba>
2019-07-24 16:04:07 +0200

    otvara se prozor za uposlenika na admin-admin

--- src/sample/LoginnRegisterController.java
+++ src/sample/LoginnRegisterController.java
@@ -1,7 +1,16 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 
 public class LoginnRegisterController {
@@ -18,5 +27,20 @@ public class LoginnRegisterController {
     }
 
     public void onLogin(ActionEvent actionEvent) {
+        if(usernameLogin.getText().equals("admin") && passwordLogin.getText().equals("admin")){
+            Parent root = null;
+            try {
+                Stage primaryStage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/employeeFile.fxml"));
+                root = loader.load();
+                primaryStage.setTitle("Employee File");
+                primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                primaryStage.initModality(Modality.APPLICATION_MODAL);
+                primaryStage.setResizable(false);
+                primaryStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
