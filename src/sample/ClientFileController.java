6a86ff2039b89109b02f09af40c4741840c2b479
alakovic1 <alakovic1@etf.unsa.ba>
2019-07-26 02:31:11 +0200

    nece filtriranje nikako...

--- src/sample/ClientFileController.java
+++ src/sample/ClientFileController.java
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableCell;
@@ -13,6 +14,9 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.ResourceBundle;
 
 public class ClientFileController implements Initializable {
@@ -47,5 +51,42 @@ public class ClientFileController implements Initializable {
         columnEngine.setCellValueFactory(new PropertyValueFactory<>("engine"));
         columnAvailable.setCellValueFactory(new PropertyValueFactory<>("available"));
         columnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
+
+    public void onComboChange(ActionEvent actionEvent) {
+        //todo ovdje detetkovati promjenu u combobox, samo skontat kako uzeti selektovanu vrijednost
+        if(boxFilter.getSelectionModel().getSelectedIndex() == 1){
+            tableofVehiclesClient.setItems(listOfVehicles);
+
+            columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+            columnBrand.setCellValueFactory(new PropertyValueFactory<>("brand"));
+            columnModel.setCellValueFactory(new PropertyValueFactory<>("model"));
+            columnDoors.setCellValueFactory(new PropertyValueFactory<>("nmbDoors"));
+            columnSeats.setCellValueFactory(new PropertyValueFactory<>("nmbSeats"));
+            columnEngine.setCellValueFactory(new PropertyValueFactory<>("engine"));
+            columnAvailable.setCellValueFactory(new PropertyValueFactory<>("available"));
+            columnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        }
+        else if(boxFilter.getSelectionModel().getSelectedIndex() == 2){
+            ObservableList<Vehicle> newList = listOfVehicles;
+            Collections.sort(newList, new Comparator<Vehicle>() {
+                @Override
+                public int compare(Vehicle o1, Vehicle o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+
+            tableofVehiclesClient.setItems(newList);
+            //todo jedino sta mi je ostalo da bazu filtriram jer ne moze ovakooo...
+
+            /*columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+            columnBrand.setCellValueFactory(new PropertyValueFactory<>("brand"));
+            columnModel.setCellValueFactory(new PropertyValueFactory<>("model"));
+            columnDoors.setCellValueFactory(new PropertyValueFactory<>("nmbDoors"));
+            columnSeats.setCellValueFactory(new PropertyValueFactory<>("nmbSeats"));
+            columnEngine.setCellValueFactory(new PropertyValueFactory<>("engine"));
+            columnAvailable.setCellValueFactory(new PropertyValueFactory<>("available"));
+            columnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));*/
+        }
     }
 }
