7c0997ade3ce505781e1c118199ea9756ccff5ce
alakovic1 <alakovic1@etf.unsa.ba>
2019-08-18 00:51:06 +0200

    dizajn admina..

--- src/sample/EmployeeFileController.java
+++ src/sample/EmployeeFileController.java
@@ -5,4 +5,13 @@ import javafx.event.ActionEvent;
 public class EmployeeFileController {
     public void onLogout(ActionEvent actionEvent) {
     }
+
+    public void onAddVehicle(ActionEvent actionEvent) {
+    }
+
+    public void onEditVehicle(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteVehicle(ActionEvent actionEvent) {
+    }
 }
